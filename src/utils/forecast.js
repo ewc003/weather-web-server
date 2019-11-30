542a77432fa616e1b42702b78e5e9e657cb063ce
Eugene <ewc003@ucsd.edu>
2019-11-30 14:17:15 -0800

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/16c839362cff372e7cac393eb4c8bd35/' + latitude + ',' + longitude + '?units=si'
+    
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('unable to connect')
+        }
+        else if (body.error) {
+            callback('unable to find location darksky')
+        }
+        else {
+            const data = body.currently
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + data.temperature + ' degrees out. There is a ' + data.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
